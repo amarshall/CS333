289ed072b3ccf498f76c2a38e5f9ab8529e04d1b
Andrew Marshall <andrew@johnandrewmarshall.com>
2010-10-25 01:13:45 -0400

    Yay multithreading!

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <pthread.h>
 #include <vector>
 
 using namespace std;
 
-struct op_arg {
+struct op_args_t {
 	int start;
 	int end;
 	int column;
@@ -18,17 +19,25 @@ inline node* add_node(node* top, vector<char>* string_ptr) {
 	new_top->next = top;
 	return new_top;
 }
+inline void op_args_init(op_args_t &op_args, int start, int end, int column) {
+	op_args.start = start;
+	op_args.end = end;
+	op_args.column = column;
+}
 
 vector< vector<char>* > strings;
 int strings_length = 0;
 const int counts_length = 127;
-node* counts[counts_length];
-vector<op_arg> stack;
+vector<op_args_t> stack;
+pthread_mutex_t stack_mutex;
+pthread_mutex_t strings_mutex;
 
-void sort_column(int start, int end, int column) {
-	for(int i=0; i < counts_length; ++i) {
-		counts[i] = NULL;
-	}
+void* sort_column(void* args) {
+	op_args_t op_args = *((op_args_t*)args);
+	int start = op_args.start;
+	int end = op_args.end;
+	int column = op_args.column;
+	node* counts[counts_length];
 	for(int i=start; i <= end; ++i) {
 		int ascii = (*strings[i])[column];
 		if(ascii != 0) {
@@ -40,7 +49,7 @@ void sort_column(int start, int end, int column) {
 	for(int i=0; i < counts_length; ++i) {
 		int runs = 0;
 		node* cur_ptr = counts[i];
-		while(cur_ptr != NULL) {			
+		while(cur_ptr != NULL) {
 			strings[cur_pos] = cur_ptr->string_ptr;
 			node* delete_me = cur_ptr;
 			cur_ptr = cur_ptr->next;
@@ -49,22 +58,37 @@ void sort_column(int start, int end, int column) {
 			++runs;
 		}
 		if(runs > 1) {
-			op_arg op_args;
-			op_args.start = cur_pos - runs;
-			op_args.end = cur_pos - 1;
-			op_args.column = column + 1;
+			op_args_t op_args;
+			op_args_init(op_args, cur_pos - runs, cur_pos - 1, column + 1);
+			pthread_mutex_lock(&stack_mutex);
 			stack.push_back(op_args);
+			pthread_mutex_unlock(&stack_mutex);
 		}
 	}
+	pthread_exit(NULL);
 }
 
 inline void sort() {
-	sort_column(0, strings_length-1, 0);
-	while(stack.size() > 0) {
-		op_arg op_args = stack.back();
+	pthread_mutex_init(&stack_mutex, NULL);
+	pthread_mutex_init(&strings_mutex, NULL);
+	op_args_t main_args;
+	op_args_init(main_args, 0, strings_length - 1, 0);
+	stack.push_back(main_args);
+	while(stack.size() >= 1) {
+		bool two = stack.size() >= 2;
+		two = false;
+		op_args_t op_args[2];
+		op_args[0] = stack.back();
 		stack.pop_back();
-		pthread
-		sort_column(op_args.start, op_args.end, op_args.column);
+		if(two) {
+			op_args[1] = stack.back();
+			stack.pop_back();
+		}
+		pthread_t thread1, thread2;
+		pthread_create(&thread1, NULL, sort_column, (void*) &op_args[0]);
+		if(two) pthread_create(&thread1, NULL, sort_column, (void*) &op_args[1]);
+		pthread_join(thread1, NULL);
+		if(two) pthread_join(thread2, NULL);
 	}
 }
 
