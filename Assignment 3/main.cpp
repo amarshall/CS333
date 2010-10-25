f44e85083d420680bd80c16f6a620a0d65c15672
Andrew Marshall <andrew@johnandrewmarshall.com>
2010-10-24 23:38:50 -0400

    Remove unused code

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream> // REMOVE ME
 #include <stdio.h>
 #include <vector>
 
@@ -12,47 +11,18 @@ struct op_arg {
 struct node {
 	node* next;
 	vector<char>* string_ptr;
-	int* op_args;
 };
-node* add_node(node* top, vector<char>* string_ptr) {
+inline node* add_node(node* top, vector<char>* string_ptr) {
 	node* new_top = new node;
 	new_top->string_ptr = string_ptr;
 	new_top->next = top;
 	return new_top;
 }
-node* add_node(node* top, int op_args[]) {
-	node* new_top = new node;
-	new_top->op_args = op_args;
-	new_top->next = top;
-	return new_top;
-}
-node* remove_top(node* top) {
-	node* new_top = top->next;
-	delete top;
-	return new_top;
-}
 
-void print_stack(node* stack) {
-	cout << "stack is:" << endl;
-	while(stack != NULL) {
-		cout << stack->op_args[0] << " | " << stack->op_args[1] << " | " << stack->op_args[2] << endl;
-		stack = stack->next;
-	}
-	cout << "end stack" << endl;
-}
-
-void print_string(vector<char>* string) {
-	for(int j=0; (*string)[j] != 0; ++j) {
-		putchar((*string)[j]);
-	}
-	putchar('\n');
-}
-
-vector< vector<char>* > strings; // Array holding all the strings
+vector< vector<char>* > strings;
 int strings_length = 0;
-node* counts[127]; // Array holding the temporary counts as a linked list of pointers
-int counts_length = 127; // Change to constant
-//node* stack;
+const int counts_length = 127;
+node* counts[counts_length];
 vector<op_arg> stack;
 
 void sort_column(int start, int end, int column) {
@@ -70,25 +40,15 @@ void sort_column(int start, int end, int column) {
 	for(int i=0; i < counts_length; ++i) {
 		int runs = 0;
 		node* cur_ptr = counts[i];
-		while(cur_ptr != NULL /* && cur_pos <= end */) {
-			/* Old: */
-			//strings[cur_pos] = *(counts[i]->data);
-			//counts[i] = remove_top(counts[i]);
-			/* end Old; New: */ // This way should be a little faster
-			
-			cout << "putting in spot " << cur_pos << ": ";
-			print_string(cur_ptr->string_ptr);
-			
+		while(cur_ptr != NULL) {			
 			strings[cur_pos] = cur_ptr->string_ptr;
-			//node* delete_me = cur_ptr;
+			node* delete_me = cur_ptr;
 			cur_ptr = cur_ptr->next;
-			//delete delete_me;
-			/* end New */
+			delete delete_me;
 			++cur_pos;
 			++runs;
 		}
 		if(runs > 1) {
-			cout << "adding " << cur_pos-runs << " | " << cur_pos - 1 << " | " << column+1 << endl;
 			op_arg op_args;
 			op_args.start = cur_pos - runs;
 			op_args.end = cur_pos - 1;
@@ -98,17 +58,17 @@ void sort_column(int start, int end, int column) {
 	}
 }
 
-void sort() {
+inline void sort() {
 	sort_column(0, strings_length-1, 0);
 	while(stack.size() > 0) {
 		op_arg op_args = stack.back();
-		cout << "running " << op_args.start << " | " << op_args.end << " | " <<  op_args.column << endl;
 		stack.pop_back();
+		pthread
 		sort_column(op_args.start, op_args.end, op_args.column);
 	}
 }
 
-void read_input() {
+inline void read_input() {
 	char c = getchar();
 	vector<char>* s = new vector<char>;
 	while(c != EOF) {
@@ -124,23 +84,18 @@ void read_input() {
 	}
 }
 
-void print_output() {
+inline void print_output() {
 	for(int i=0; i < strings_length; ++i) {
-		vector<char>* string = strings[i];
-		for(int j=0; (*string)[j] != 0; ++j) {
-			putchar((*string)[j]);
+		for(int j=0; (*strings[i])[j] != 0; ++j) {
+			putchar((*strings[i])[j]);
 		}
 		putchar('\n');
 	}
 }
 
 int main() {
-	/*
-	for(int i=0; i < counts_length; ++i) {
-		counts[i] = NULL;
-	}
-	*/
 	read_input();
 	sort();
 	print_output();
+	return 0;
 }
